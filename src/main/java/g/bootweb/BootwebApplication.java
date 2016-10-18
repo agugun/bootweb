9c6dffa1215b9dae591ed71261d369860ee8741b
AnggunG <AnggunG@PB03252W.corp.vico.co.id>
2016-10-18 15:15:47 +0700

    init

---
+++ src/main/java/g/bootweb/BootwebApplication.java
@@ -0,0 +1,12 @@
+package g.bootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootwebApplication.class, args);
+	}
+}
