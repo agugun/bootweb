9f1341ddb7d76e60150c1fbe8d87a9ea409e8140
AnggunG <AnggunG@PB03252W.corp.vico.co.id>
2016-10-19 15:14:40 +0700

    update ui

--- src/main/java/g/bootweb/web/HelloController.java
+++ src/main/java/g/bootweb/web/HelloController.java
@@ -1,14 +1,18 @@
 package g.bootweb.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloController {
 	
-	@RequestMapping("/test")
-    public String index() {
-        return "index.html";
+	@RequestMapping("/input")
+    public String index(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
+		return "input";
     }
 
 }
